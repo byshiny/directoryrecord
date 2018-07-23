42c9ec6754bf6f1f0fed2b318155794f683f6e56
byshiny <bs5ht@virginia.edu>
2018-07-22 20:44:46 -0400

    successfully stored path via fopen

--- directoryrecord.c
+++ directoryrecord.c
@@ -5,29 +5,42 @@
 
 int main(int argc, char *argv[])   //  command line arguments
 {
-if(argc!=5)
-{
-   printf("Arguments passed through command line " \
-          "not equal to 5");
-   return 1;
-}
 
-   printf("\n Program name  : %s \n", argv[0]);
-   printf("1st arg  : %s \n", argv[1]);
-   printf("2nd arg  : %s \n", argv[2]);
-   printf("3rd arg  : %s \n", argv[3]);
-   printf("4th arg  : %s \n", argv[4]);
-   printf("5th arg  : %s \n", argv[5]);
+        //char * cwd_out = popen ("pwd", "r");
 
-return 0;
+        char buf[100];
+        char *str = NULL;
+        char *temp = NULL;
+        unsigned int size = 1; // start with size of 1 to make room for null terminator
+        unsigned int strlength;
+
+        FILE *fp;
+        char path[1035];
+
+/* Open the command for reading. */
+        //fp = popen("/bin/ls /etc/", "r");
+        fp = popen("pwd", "r");
+        if (fp == NULL) {
+                printf("Failed to run command\n" );
+                exit(1);
+        }
+
+/* Read the output a line at a time - output it. */
+        while (fgets(path, sizeof(path)-1, fp) != NULL) {
+                printf("%s", path);
+        }
+
+/* close */
+        pclose(fp);
+        printf("Hello, World\n"); // \n indicates a newline character
 }
 void printDirectoryHistory(){
 
-  
+
 }
 
 /*
-void get_popen()
+   void get_popen()
     FILE *pf;
     char command[20];
     char data[512];
@@ -49,5 +62,5 @@ void get_popen()
         fprintf(stderr," Error: Failed to close command stream \n");
 
     return;
-}
-*/
+   }
+ */
